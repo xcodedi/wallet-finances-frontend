98f91197a9da3234befb65b7456ed6b1795a18d3
xcodedi <di.meira@hotmail.com>
2025-01-25 15:27:39 -0300

    get email,id,name from API

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-const onclicklogin = () => {
+const validateUser = async (email) => {
+  try {
+    const result = await fetch("");
+    const user = await result.json();
+    return user;
+  } catch (error) {
+    return { error };
+  }
+};
+
+const onclicklogin = async () => {
   const email = document.getElementById("input-email").value;
+
   if (email.length < 5 || !email.includes("@")) {
     alert("Invalid email");
     return;
   }
-  console.log(email);
-  localStorage.setItem("userEmail", email);
 
-  window.location.href = "./src/pages/home/index.html";
+  console.log(email);
+
+  try {
+    const result = await validateUser(email);
+    console.log(result);
+
+    if (result.error) {
+      alert("Fail to validate email");
+      return;
+    }
+
+    const { name, id } = result;
+    if (!name || !id) {
+      alert("Failed to retrieve user details");
+      return;
+    }
+
+    localStorage.setItem("userEmail", email);
+    localStorage.setItem("username", name);
+    localStorage.setItem("userid", id);
+
+    window.location.href = "./src/pages/home/index.html";
+  } catch (error) {
+    console.error("An unexpected error occurred:", error);
+    alert("An error occurred during login. Please try again.");
+  }
 };
